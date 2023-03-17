5c3b5c72c7ad578221fd1a4e239def20c9230bfc
Barrett Williamson <williamsonbarrett@gmail.com>
2023-03-17 16:44:57 -0400

    switched to yt-dlp as pytube was not working

--- summary.py
+++ summary.py
@@ -1,12 +1,14 @@
 import openai
 import whisper
-import pytube
+import yt_dlp
 import os
+from dotenv import load_dotenv
+import sys
 
 openai.api_key = os.getenv('OPENAI_TOKEN')
 model = whisper.load_model("base")
 
-def generate_response(self, prompt):
+def generate_response(prompt):
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=prompt,
@@ -17,14 +19,32 @@ def generate_response(self, prompt):
         )
         return response.choices[0].text.strip()
 
-video = "https://www.youtube.com/watch?v=x7X9w_GIm1s"
-data = pytube.YouTube(video)
+def summary(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': 'audio.%(ext)s',
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
-# Convert audio to mp4
-audio = data.streams.get_audio_only()
-audio.download(filename="audio.mp3")
+    # Transcribe video with whisper
+    text = model.transcribe("audio.mp3")
+    textstr = text["text"]
+    with open("transcript.txt", "w") as f:
+        f.write(textstr)
+        f.close()
 
-# Transcribe video with whisper
-text = model.transcribe()
+    print(generate_response(f"Provide a summary of the following \n {textstr}"))
 
-generate_response(f"Could you provide a summary of the following? \n {text}")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python summary.py [YouTube_URL]")
+    else:
+        url = sys.argv[1]
+        summary(url)
